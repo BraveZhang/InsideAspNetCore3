19d634ce466afe4f9edf97badc39cacf8a508a61
BraveZhang <479893438@qq.com>
2022-12-10 17:38:02 +0800

    S409学习

--- 01/04/S409/App/Program.cs
+++ 01/04/S409/App/Program.cs
@@ -14,6 +14,8 @@ namespace App
                 .AddTransient<IQux, Qux>()
                 .BuildServiceProvider()
                 .GetServices<IQux>();
+
+            Console.ReadLine();
         }
     }
 }
