bc594d4f58355502536e1ae4b5bfae485f7b614a
BraveZhang <479893438@qq.com>
2023-08-15 00:01:41 +0800

    S614，有两个输出结果比较奇怪

--- 01/06/S614/App/Program.cs
+++ 01/06/S614/App/Program.cs
@@ -8,6 +8,14 @@ namespace App
     {
         static void Main(string[] args)
         {
+            // dotnet run architecture=x64                  Architecture: x64
+            // dotnet run /architecture=x64                 Architecture: x64
+            // dotnet run --architecture=x64                Architecture: x64
+            // dotnet run -architecture=x64                 Error: The short switch '-architecture=x64' is not defined in the switch mappings.
+            // dotnet run -a=x64                            Architecture:
+            // dotnet run -arch=x64                         Architecture: x64
+            // dotnet run -a x64                            Architecture:
+            // dotnet run -arch x64                         Architecture: x64
             try
             {
                 var mapping = new Dictionary<string, string>
@@ -18,7 +26,9 @@ namespace App
                 var configuration = new ConfigurationBuilder()
                     .AddCommandLine(args, mapping)
                     .Build();
+
                 Console.WriteLine($"Architecture: {configuration["architecture"]}");
+
             }
             catch (Exception ex)
             {
