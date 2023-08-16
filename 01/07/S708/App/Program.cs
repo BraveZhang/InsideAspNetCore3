f4c7470fc10ec0a27fae3567256623a74b529287
BraveZhang <479893438@qq.com>
2023-08-16 23:27:52 +0800

    S708学习

--- 01/07/S708/App/Program.cs
+++ 01/07/S708/App/Program.cs
@@ -8,43 +8,44 @@ using System.Globalization;
 
 namespace App
 {
-class Program
-{
-    public static void Main(string[] args)
+    class Program
     {
-        var config = new ConfigurationBuilder()
-            .AddCommandLine(args)
-            .Build();
-        var datePattern = config["date"];
-        var timePattern = config["time"];
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+            var datePattern = config["date"];
+            var timePattern = config["time"];
 
-        var services = new ServiceCollection();
-        services.AddOptions<DateTimeFormatOptions>()
-            .Configure(options =>
+            var services = new ServiceCollection();
+            services.AddOptions<DateTimeFormatOptions>()
+                .Configure(options =>
+                {
+                    options.DatePattern = datePattern;
+                    options.TimePattern = timePattern;
+                })
+                // 进行Validate
+                .Validate(options => Validate(options.DatePattern) && Validate(options.TimePattern), "Invalid Date or Time pattern.");
+
+            try
             {
-                options.DatePattern = datePattern;
-                options.TimePattern = timePattern;
-            })
-            .Validate(options=>Validate(options.DatePattern) && Validate(options.TimePattern),"Invalid Date or Time pattern.");
+                var options = services
+                    .BuildServiceProvider()
+                    .GetRequiredService<IOptions<DateTimeFormatOptions>>().Value;
+                Console.WriteLine(options);
+            }
+            catch (OptionsValidationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
-        try
-        {
-            var options = services
-                .BuildServiceProvider()
-                .GetRequiredService<IOptions<DateTimeFormatOptions>>().Value;
-            Console.WriteLine(options);
-        }
-        catch (OptionsValidationException ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-
-        static bool Validate(string format)
-        {
-            var time = new DateTime(1981, 8, 24,2,2,2);
-            var formatted = time.ToString(format);
-            return DateTimeOffset.TryParseExact(formatted, format, CultureInfo.CurrentCulture, DateTimeStyles.None, out var value) && (value.Date == time.Date || value.TimeOfDay == time.TimeOfDay);
+            static bool Validate(string format)
+            {
+                var time = new DateTime(1981, 8, 24, 2, 2, 2);
+                var formatted = time.ToString(format);
+                return DateTimeOffset.TryParseExact(formatted, format, CultureInfo.CurrentCulture, DateTimeStyles.None, out var value) && (value.Date == time.Date || value.TimeOfDay == time.TimeOfDay);
+            }
         }
     }
-}
 }
