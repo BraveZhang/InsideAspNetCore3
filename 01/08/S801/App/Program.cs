5afe4561eb718db66b21f9cee90732775d760f8b
BraveZhang <479893438@qq.com>
2023-08-21 23:13:49 +0800

    S801学习

--- 01/08/S801/App/Program.cs
+++ 01/08/S801/App/Program.cs
@@ -7,7 +7,7 @@ namespace App
     {
         static void Main()
         {
-            var source = new TraceSource("Foobar", SourceLevels.All);
+            var source = new TraceSource("Foobar", SourceLevels.All);// SourceLevels.All表示最低等级及以上都会记录
             var eventTypes = (TraceEventType[])Enum.GetValues(typeof(TraceEventType));
             var eventId = 1;
             Array.ForEach(eventTypes, it => source.TraceEvent(it, eventId++, $"This is a {it} message."));
