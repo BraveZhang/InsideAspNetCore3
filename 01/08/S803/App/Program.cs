7978cc741d2976708d3a5300c7eb8415328ad4fb
BraveZhang <479893438@qq.com>
2023-08-21 23:18:30 +0800

    S803学习

--- 01/08/S803/App/Program.cs
+++ 01/08/S803/App/Program.cs
@@ -8,7 +8,7 @@ namespace App
         static void Main()
         {
             var source = new TraceSource("Foobar", SourceLevels.Warning);
-            source.Listeners.Add(new ConsoleTraceListener());
+            source.Listeners.Add(new ConsoleTraceListener());// 自定义Listener
             var eventTypes = (TraceEventType[])Enum.GetValues(typeof(TraceEventType));
             var eventId = 1;
             Array.ForEach(eventTypes, it => source.TraceEvent(it, eventId++, $"This is a {it} message."));
