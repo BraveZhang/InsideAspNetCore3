d3671844c8eb78ab49eb257b625db5d18c4e9436
BraveZhang <479893438@qq.com>
2023-09-23 17:41:42 +0800

    S814学习

--- 01/08/S814/App/Program.cs
+++ 01/08/S814/App/Program.cs
@@ -12,18 +12,18 @@ namespace App
         {
             DiagnosticListener.AllListeners.Subscribe(listener =>
             {
-                if (listener.Name == "Web")
+                if (listener.Name == "Web")// 过滤Web
                 {
                     listener.Subscribe(eventData =>
                     {
-                        if (eventData.Key == "ReceiveRequest")
+                        if (eventData.Key == "ReceiveRequest")// 过滤Web下的ReceiveRequest事件
                         {
                             dynamic payload = eventData.Value;
                             var request = (HttpRequestMessage)(payload.Request);
                             var timestamp = (long)payload.Timestamp;
                             Console.WriteLine($"Receive request. Url: {request.RequestUri}; Timstamp:{ timestamp}");
                         }
-                        if (eventData.Key == "SendReply")
+                        if (eventData.Key == "SendReply")// 过滤Web下的SendReply事件
                         {
                             dynamic payload = eventData.Value;
                             var response = (HttpResponseMessage)(payload.Response);
@@ -34,19 +34,19 @@ namespace App
                 }
             });
 
-            var source = new DiagnosticListener("Web");
+            var source = new DiagnosticListener("Web"); // 定义Web
             var stopwatch = Stopwatch.StartNew();
             if (source.IsEnabled("ReceiveRequest"))
             {
                 var request = new HttpRequestMessage(HttpMethod.Get, "https://www.artech.top");
-                source.Write("ReceiveRequest", new
+                source.Write("ReceiveRequest", new// 发送Web下的ReceiveRequest事件
                 {
                     Request = request,
                     Timestamp = Stopwatch.GetTimestamp()
                 });
             }
             Task.Delay(100).Wait();
-            if (source.IsEnabled("SendReply"))
+            if (source.IsEnabled("SendReply"))// 发送Web下的SendReply事件
             {
                 var response = new HttpResponseMessage(HttpStatusCode.OK);
                 source.Write("SendReply", new
