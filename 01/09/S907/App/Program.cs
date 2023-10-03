00d171e7d348b2e014ef3e230232e6b651a79834
BraveZhang <479893438@qq.com>
2023-10-03 15:31:25 +0800

    S907学习

--- 01/09/S907/App/Program.cs
+++ 01/09/S907/App/Program.cs
@@ -7,6 +7,8 @@ using System.Diagnostics.Tracing;
 using Microsoft.Extensions.Logging.Console;
 using Microsoft.Extensions.Logging.Debug;
 using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic;
+using System.Security.Policy;
 
 namespace App
 {
@@ -20,7 +22,7 @@ namespace App
 
             var loggerFactory = new ServiceCollection()
                 .AddLogging(builder => builder
-                    .AddConfiguration(configuration)
+                    .AddConfiguration(configuration)// 基于配置的方式
                     .AddConsole()
                     .AddDebug())
                 .BuildServiceProvider()
@@ -30,17 +32,26 @@ namespace App
             var barLogger = loggerFactory.CreateLogger("Bar");
             var bazLogger = loggerFactory.CreateLogger("Baz");
 
+            // LogLevel:Trace = 0,Debug = 1,Information = 2,Warning = 3,Error = 4,Critical = 5,None = 6
             var levels = (LogLevel[])Enum.GetValues(typeof(LogLevel));
             levels = levels.Where(it => it != LogLevel.None).ToArray();
 
+            // foo-console:Warning,Error,Critical
+            // foo-debug:Debug,Information,Warning,Error,Critical
             var eventId = 1;
             Array.ForEach(levels, level => fooLogger.Log(level, eventId++, "This is a/an {0} log message.", level));
 
+            // bar-console:Error,Critical
+            // bar-debug:Error,Critical
             eventId = 1;
             Array.ForEach(levels, level => barLogger.Log(level, eventId++, "This is a/an {0} log message.", level));
 
+            // baz-console:Information,Warning,Error,Critical
+            // baz-debug:Error,Critical
             eventId = 1;
             Array.ForEach(levels, level => bazLogger.Log(level, eventId++, "This is a/an {0} log message.", level));
+
+
             Console.Read();
         }
     }
