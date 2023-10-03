2ce5c8bc8240c64e67d2046e5c8be07f03e0638a
BraveZhang <479893438@qq.com>
2023-10-03 12:02:19 +0800

    S906学习

--- 01/09/S906/App/Program.cs
+++ 01/09/S906/App/Program.cs
@@ -9,39 +9,39 @@ using Microsoft.Extensions.Logging.Debug;
 
 namespace App
 {
-public class Program
-{
-    public static void Main()
+    public class Program
     {
-        var logger = new ServiceCollection()
-            .AddLogging(builder => builder
-                .AddFilter(Filter)
-                .AddConsole()
-                .AddDebug())
-            .BuildServiceProvider()
-            .GetRequiredService<ILogger<Program>>();
-
-        var levels = (LogLevel[])Enum.GetValues(typeof(LogLevel));
-        levels = levels.Where(it => it != LogLevel.None).ToArray();
-        var eventId = 1;
-        Array.ForEach(levels, level => logger.Log(level, eventId++, "This is a/an {0} log message.", level));
-        Console.Read();
-
-        static bool Filter(string provider, string category, LogLevel level)
+        public static void Main()
         {
-            if (provider == typeof(ConsoleLoggerProvider).FullName)
+            var logger = new ServiceCollection()
+                .AddLogging(builder => builder
+                    .AddFilter(Filter)
+                    .AddConsole()
+                    .AddDebug())
+                .BuildServiceProvider()
+                .GetRequiredService<ILogger<Program>>();
+
+            var levels = (LogLevel[])Enum.GetValues(typeof(LogLevel));
+            levels = levels.Where(it => it != LogLevel.None).ToArray();
+            var eventId = 1;
+            Array.ForEach(levels, level => logger.Log(level, eventId++, "This is a/an {0} log message.", level));
+            Console.Read();
+
+            static bool Filter(string provider, string category, LogLevel level)
             {
-                return level >= LogLevel.Debug;
-            }
-            else if (provider == typeof(DebugLoggerProvider).FullName)
-            {
-                return level >= LogLevel.Warning;
-            }
-            else
-            {
-                return true;
+                if (provider == typeof(ConsoleLoggerProvider).FullName)
+                {
+                    return level >= LogLevel.Debug;
+                }
+                else if (provider == typeof(DebugLoggerProvider).FullName)
+                {
+                    return level >= LogLevel.Warning;
+                }
+                else
+                {
+                    return true;
+                }
             }
         }
     }
-}
 }
