97619cdcdeec7237e858121e382d74c242ccc787
BraveZhang <479893438@qq.com>
2023-11-05 17:59:28 +0800

    S1008学习

--- 01/10/S1008/App/Program.cs
+++ 01/10/S1008/App/Program.cs
@@ -9,12 +9,13 @@ namespace App
     {
         static void Main()
         {
+            // 承载系统针对依赖注入框架的设置例子，注意依赖注入使用了之前学到的Cat例子
             new HostBuilder()
-                .ConfigureServices(svcs => svcs.AddHostedService<FakeHostedService>())
-                .UseServiceProviderFactory(new CatServiceProviderFactory())
-                .ConfigureContainer<CatBuilder>(builder => builder.Register(Assembly.GetEntryAssembly()))
-                .Build()
-                .Run();
+                .ConfigureServices(svcs => svcs.AddHostedService<FakeHostedService>())// 注册自定义的FakeHostedService服务
+                .UseServiceProviderFactory(new CatServiceProviderFactory())// 使用依赖注入章节的CatServiceProviderFactory自定义工厂
+                .ConfigureContainer<CatBuilder>(builder => builder.Register(Assembly.GetEntryAssembly()))// 对依赖注入的容器做进一步设置
+                .Build()// 创建IHost对象
+                .Run();// 运行承载系统
         }
     }
 
