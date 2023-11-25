70c7cbad39ef6faa7c343ca93160b5d6dd296d50
BraveZhang <479893438@qq.com>
2023-11-25 15:39:03 +0800

    S1101学习

--- 01/11/S1101/App/Program.cs
+++ 01/11/S1101/App/Program.cs
@@ -6,28 +6,29 @@ using Microsoft.Extensions.Hosting;
 namespace App
 {
     class Program
-{
-    static void Main()
     {
-        static RequestDelegate Middleware1(RequestDelegate next) 
-            => async context =>
-            {
-                await context.Response.WriteAsync("Hello");
-                await next(context);
-            };
-        static RequestDelegate Middleware2(RequestDelegate next) 
-             => async context =>
-            {
-                await context.Response.WriteAsync(" World!");
-            };
+        static void Main()
+        {
+            static RequestDelegate Middleware1(RequestDelegate next)
+                => async context =>
+                {
+                    await context.Response.WriteAsync("Hello");
+                    await next(context);// 会调用Middleware2的实现，如何执行？
+                };
+            static RequestDelegate Middleware2(RequestDelegate next)
+                 => async context =>
+                {
+                    await context.Response.WriteAsync(" World!");
+                };
 
-        Host.CreateDefaultBuilder().ConfigureWebHostDefaults(builder => builder
-            .Configure(app => app
-                .Use(Middleware1)
-                .Use(Middleware2)))
-        .Build()
-        .Run();
+            Host.CreateDefaultBuilder().ConfigureWebHostDefaults(builder => builder
+                .Configure(app => app
+                    .Use(Middleware1)// Use内部如何实现？
+                    .Use(Middleware2))
+                )
+            .Build()
+            .Run();
+        }
     }
-}
 
 }
