b32dac9e2297f4530271f7aa27ec79fe43f378eb
zhangyong <479893438@qq.com>
2024-04-23 17:28:55 +0800

    S1603学习

--- 02/16/S1603/App/Program.cs
+++ 02/16/S1603/App/Program.cs
@@ -12,15 +12,23 @@ namespace App
     {
         public static void Main()
         {
+            // 1个或多个中间件参与异常处理
             Host.CreateDefaultBuilder()
                 .ConfigureWebHostDefaults(builder => builder.Configure(app => app
-                    .UseExceptionHandler(app2 => app2.Run(HandleAsync))
-                    .Run(context => Task.FromException(new InvalidOperationException("Manually thrown exception...")))))
+                    .UseExceptionHandler(app2 =>// UseExceptionHandler方法重载
+                    {
+                        app2.Run(HandleAsync);
+                    }
+                    )
+                    .Run(context =>
+                    {
+                        return Task.FromException(new InvalidOperationException("Manually thrown exception..."));// 1
+                    })))
                 .Build()
                 .Run();
 
             static Task HandleAsync(HttpContext context)
-                => context.Response.WriteAsync("Unhandled exception occurred!");
+                => context.Response.WriteAsync("Unhandled exception occurred!");// 2
         }
     }
 }
